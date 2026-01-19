a9cb0a5e6f8852f597fa3cd305b9ad806686d64b
Ethan Thoma <ethanthoma@gmail.com>
2026-01-19 11:01:57 -0800

    feat: modernise flake

--- flake.nix
+++ flake.nix
@@ -1,54 +1,87 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    bacon-ls = {
+      url = "github:crisidev/bacon-ls";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs
-    , flake-utils
-    , ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+    inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devshell.flakeModule ];
 
-        buildInputs = with pkgs; [
-          expat
-          fontconfig
-          freetype
-          freetype.dev
-          libGL
-          pkg-config
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-          wayland
-          libxkbcommon
-          rustc
-          cargo
-          clippy
-          rust-analyzer
-          rustfmt
-          bacon
-          lazygit
-          diesel-cli
-          openssl
-          watchexec
-          websocat
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-          LD_LIBRARY_PATH =
-            builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          guiLibs = with pkgs; [
+            libGL
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+            wayland
+            libxkbcommon
+            fontconfig
+            freetype
+            expat
+          ];
+        in
+        {
+          devshells.default = {
+            packages = [
+              (inputs.rust-overlay.lib.mkRustBin { } pkgs).stable.latest.default
+              pkgs.rust-analyzer
+              pkgs.bacon
+              pkgs.taplo
+              inputs.bacon-ls.defaultPackage.${system}
+              pkgs.mold
+              pkgs.pkg-config
+              pkgs.lazygit
+              pkgs.websocat
+              pkgs.diesel-cli
+              pkgs.openssl
+              pkgs.watchexec
+            ]
+            ++ guiLibs;
+
+            env = [
+              {
+                name = "RUSTFLAGS";
+                value = "-C link-arg=-fuse-ld=mold";
+              }
+              {
+                name = "LD_LIBRARY_PATH";
+                value = pkgs.lib.makeLibraryPath guiLibs;
+              }
+            ];
+          };
         };
-      }
-    );
+    };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org?priority=1"
+      "https://numtide.cachix.org?priority=2"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+  };
 }
