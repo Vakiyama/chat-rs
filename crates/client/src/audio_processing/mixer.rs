1e70292b6bac00e02acd23e4e47db718830ec447
Ethan Thoma <ethanthoma@gmail.com>
2026-06-13 00:02:56 -0700

    feat: jitter! (#19)

--- crates/client/src/audio_processing/mixer.rs
+++ crates/client/src/audio_processing/mixer.rs
@@ -3,33 +3,72 @@ use std::{
   sync::{Arc, Mutex},
 };
 
-#[derive(Default, Clone)]
-pub struct Mixer(Arc<Mutex<HashMap<u32, VecDeque<f32>>>>);
+#[derive(Default)]
+struct Source {
+  queue: VecDeque<f32>,
+  primed: bool,
+  last: f32,
+}
+
+#[derive(Clone)]
+pub struct Mixer {
+  sources: Arc<Mutex<HashMap<u32, Source>>>,
+  target: usize,
+  max: usize,
+}
 
 // we have multiple incoming audio tracks from peers in a voice call,
 // we'd like to mix them all into a single output track and play that
 impl Mixer {
+  pub fn new(sample_rate: u32) -> Self {
+    Self {
+      sources: Default::default(),
+      target: sample_rate as usize * 40 / 1000, // 40ms prebuffer
+      max: sample_rate as usize * 200 / 1000,   // 200ms
+    }
+  }
+
   pub fn push(&self, src: u32, samples: &[f32]) {
-    let mut mixer = self.0.lock().unwrap();
-    let queue = mixer.entry(src).or_default();
-    queue.extend(samples);
+    let mut sources = self.sources.lock().unwrap();
+    let source = sources.entry(src).or_default();
+    source.queue.extend(samples);
+
+    if source.queue.len() >= self.target {
+      source.primed = true;
+    }
 
     // drops packets from queue if we back up past 200 ms
-    while queue.len() > 48_000 / 5 {
-      queue.pop_front();
+    while source.queue.len() > self.max {
+      source.queue.drain(..source.queue.len() - self.target);
     }
   }
 
   pub fn remove(&self, src: u32) {
-    self.0.lock().unwrap().remove(&src);
+    self.sources.lock().unwrap().remove(&src);
   }
 
   pub fn mix_mono(&self, out: &mut [f32]) {
-    let mut m = self.0.lock().unwrap();
-    for s in out.iter_mut() {
-      *s = m
+    let mut sources = self.sources.lock().unwrap();
+
+    for sample in out.iter_mut() {
+      *sample = sources
         .values_mut()
-        .filter_map(|q| q.pop_front())
+        .map(|source| {
+          if !source.primed {
+            return 0.0; // silent until prefilling finished
+          }
+          match source.queue.pop_front() {
+            Some(sample) => {
+              source.last = sample;
+              sample
+            }
+            None => {
+              source.primed = false; // prefill again
+              source.last *= 0.85;
+              source.last
+            }
+          }
+        })
         .sum::<f32>()
         .clamp(-1.0, 1.0);
     }
